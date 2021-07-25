17965e30e38969717a54d28e42332f374a5d88a9
lakub-muravlov <lakub.muravlov@gmail.com>
2021-07-26 01:15:59 +0300

    main screen

--- src/views/Queue.vue
+++ src/views/Queue.vue
@@ -1,32 +1,36 @@
 <template>
-	<div class="queue-page">
-		<div class="container">
-			<div class="logo-wrap">
-				<h3>IASA</h3>
-				<img src="../../public/img/logo.svg" alt="" />
-			</div>
-			<div class="link-wrap">
-				<router-link to="/queue">Подивитися всю чергу</router-link>
-			</div>
-			<div class="table-wrap">
-				<div class="table" v-for="(q, idx) of que" :key="q.id">
-					<h4>СТІЛ №{{ idx + 1 }}</h4>
-					<p class="number">{{ q | number(q) }}</p>
-				</div>
-				<div class="table next mt">
-					<h4>Next</h4>
-					<p class="number">{{ next | number(next) }}</p>
-				</div>
-			</div>
-			<div class="time">
-				<p>Середній час обробки</p>
-				<p class="mins">{{ avgTime }} хв.</p>
-			</div>
-			<div class="audio">
-				<audio controls ref="music" src="../../public/audio/a.mp3"></audio>
-			</div>
-		</div>
-	</div>
+  <div class="queue-page">
+    <div class="container">
+      <div class="logo-wrap">
+        <h3>IASA</h3>
+        <img src="../../public/img/logo.svg" alt="">
+      </div>
+      <div class="link-wrap">
+        <router-link to="/queue">Подивитися всю чергу</router-link>
+      </div>
+      <div class="table-wrap jc-sb">
+        <div
+            class="table fd-column"
+            v-for="(q, idx) of que"
+            :key="q.id"
+        >
+          <h4>СТІЛ №{{idx + 1}}</h4>
+          <p class="number">{{q | number(q)}}</p>
+        </div>
+        <div class="table next mt fd-column">
+          <h4 class="not-fixble">Next</h4>
+          <p class="number">{{next | number(next)}}</p>
+        </div>
+      </div>
+      <div class="time">
+        <p>Середній час обробки</p>
+        <p class="mins">{{ avgTime }} хв.</p>
+      </div>
+      <div class="audio">
+        <audio controls ref="music" src="../../public/audio/a.mp3"></audio>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
