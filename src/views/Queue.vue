c69da5d0e0bfee64b729292b6914f080f1d97afa
lakub-muravlov <lakub.muravlov@gmail.com>
2021-07-25 17:47:36 +0300

    setting up file stracture

---
+++ src/views/Queue.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Queue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/QueueFull.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "QueueFull"
+}
+</script>
+
+<style scoped>
+
+</style>
